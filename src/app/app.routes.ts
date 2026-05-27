3de026eda3d1771ad445f97b7a7591ca962c8295
mchalop2803 <mchalop2803@iescarrillo.es>
2026-05-27 18:01:34 +0200

    fix rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,49 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Home } from './pages/home/home';
 
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+
+import { Home } from './pages/home/home';
 import { Medications } from './pages/medications/medications';
 import { Citas } from './pages/citas/citas';
 import { Alerts } from './pages/alerts/alerts';
-
-import { Register } from './pages/register/register';
 
 import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
 
+  // =========================
+  // PUBLIC
+  // =========================
   { path: 'login', component: Login },
   { path: 'register', component: Register },
 
+  // =========================
+  // APP (PROTEGIDA)
+  // =========================
   {
     path: '',
     component: DashboardLayoutComponent,
     canActivate: [authGuard],
     children: [
+
       { path: 'home', component: Home },
 
       { path: 'medications', component: Medications },
+
       { path: 'citas', component: Citas },
+
       { path: 'alerts', component: Alerts },
+
+      { path: 'alerts/new/:id', component: Alerts },
 
       { path: '', redirectTo: 'medications', pathMatch: 'full' }
     ]
   },
 
+  // =========================
+  // FALLBACK
+  // =========================
   { path: '**', redirectTo: 'login' }
 ];
