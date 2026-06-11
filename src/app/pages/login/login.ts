a644eca3f20dc4de1725ba8dac1d36b2194c4a2c
mchalop2803 <mchalop2803@iescarrillo.es>
2026-06-11 17:45:16 +0200

    mensajes informativo

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -25,19 +25,51 @@ export class Login {
   ) {}
 
   login() {
+
     this.error = '';
     this.loading = true;
 
     this.auth.login(this.email, this.password)
       .then(() => {
+
         this.router.navigate(['/home']);
+
       })
-      .catch((err) => {
+      .catch((err: any) => {
+
         console.error(err);
-        this.error = 'Email o contraseña incorrectos';
+
+        switch (err.code) {
+
+          case 'auth/user-not-found':
+            this.error = 'No existe ninguna cuenta con este correo';
+            break;
+
+          case 'auth/wrong-password':
+            this.error = 'La contraseña es incorrecta';
+            break;
+
+          case 'auth/invalid-email':
+            this.error = 'El formato del correo no es válido';
+            break;
+
+          case 'auth/invalid-credential':
+            this.error = 'Correo o contraseña incorrectos';
+            break;
+
+          case 'auth/too-many-requests':
+            this.error = 'Demasiados intentos. Inténtalo más tarde';
+            break;
+
+          default:
+            this.error = 'Error al iniciar sesión';
+        }
+
       })
       .finally(() => {
+
         this.loading = false;
+
       });
   }
 
